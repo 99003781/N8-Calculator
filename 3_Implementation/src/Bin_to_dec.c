ca1f026440c5df293eca1c907947998daffee0c1
99003780 <78890690+99003780@users.noreply.github.com>
2021-02-15 14:09:13 +0530

    Update Bin_to_dec.c

--- 3_Implementation/src/Bin_to_dec.c
+++ 3_Implementation/src/Bin_to_dec.c
@@ -1,31 +1,21 @@
 #include <math.h>
 #include <stdio.h>
-int bintooct(long long b);
-int main() 
-{
-    long long b;
+int bintodec(long long n);
+int main() {
+    long long num;
     printf("Enter a binary number: ");
-    scanf("%lld", &b);
-    printf("%lld in binary = %d in octal", b, bintooct(b));
+    scanf("%lld", &num);
+    printf("%lld in binary = %d in decimal", num, bintodec(num));
     return 0;
 }
 
-int bintooct(long long b) 
-{
-    int o = 0, d = 0, t = 0;
-
-    // converting binary to decimal
-    while (b != 0) {
-        d += (b % 10) * pow(2, t);
+int bintodec(long long num) {
+    int dec = 0, d = 0, res;
+    while (num != 0) {
+        res = num % 10;
+        num /= 10;
+        dec += res * pow(2, d);
         ++d;
-        b /= 10;
     }
-    t = 1;
-     // converting to decimal to octal
-    while (d != 0) {
-        o += (d % 8) * t;
-        d /= 8;
-        t *= 10;
-    }
-    return o;
+    return dec;
 }
