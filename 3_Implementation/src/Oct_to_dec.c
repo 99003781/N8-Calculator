703f563007eacc05a867f0c085f8f047191affd2
99003780 <78890690+99003780@users.noreply.github.com>
2021-02-15 12:52:58 +0530

    Create Oct_to_dec.c

---
+++ 3_Implementation/src/Oct_to_dec.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+long octtodec(int oct);
+int main()
+{
+    int oct;
+    printf("Enter an octal number: ");
+    scanf("%d", &oct);
+    printf("%d oct  = %ld decimal", oct, octtodec(oct));
+    return 0;
+}
+long octtodec (int oct)
+{
+    int dec = 0, d = 0;
+    while(oct != 0)
+    {
+        dec += (oct%10) * pow(8,d);
+        ++d;
+        oct/=10;
+    }
+    d = 1; 
+ return dec;
+}
