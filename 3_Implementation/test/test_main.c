4cd18bc39628dbc7780f06bb025fece27b0475d0
99003781 <78873487+99003781@users.noreply.github.com>
2021-03-12 15:08:17 +0530

    Update test_main.c

--- 3_Implementation/test/test_main.c
+++ 3_Implementation/test/test_main.c
@@ -1,10 +1,51 @@
 #include "unity.h"
 #include "header_main.h"
 
-//####################trigonometary############
+//###################Trigonometary###################
 
+      //**********test cases for Degree_to_Radaian conversion************
 
-void test_get_user_choice(void)
-{
-     TEST_ASSERT_EQUAL(1,get_user_choice(selected_choice));
-}
+          //^^^^^^^^^^^^^^^^^^^^^^^^for 0 Degree^^^^^^^^^^^^^^^^^^^^^^^^
+
+          float Degree_to_Radaian_sin_0(float degree);
+          float Degree_to_Radaian_cos_0(float degree);
+          float Degree_to_Radaian_tan_0(float degree);
+          float Degree_to_Radaian_cosec_0(float degree);
+          float Degree_to_Radaian_sec_0(float degree);
+          float Degree_to_Radaian_cot_0(float degree);
+
+ //^^^^^^^^^^^^^^^^^^^^^^^^for 30 Degree^^^^^^^^^^^^^^^^^^^^^^^^
+
+          float Degree_to_Radaian_sin_30(float degree);
+          float Degree_to_Radaian_cos_30(float degree);
+          float Degree_to_Radaian_tan_30(float degree);
+          float Degree_to_Radaian_cosec_30(float degree);
+          float Degree_to_Radaian_sec_30(float degree);
+          float Degree_to_Radaian_cot_30(float degree);
+
+ //^^^^^^^^^^^^^^^^^^^^^^^^for 45 Degree^^^^^^^^^^^^^^^^^^^^^^^^
+
+          float Degree_to_Radaian_sin_45(float degree);
+          float Degree_to_Radaian_cos_45(float degree);
+          float Degree_to_Radaian_tan_45(float degree);
+          float Degree_to_Radaian_cosec_45(float degree);
+          float Degree_to_Radaian_sec_45(float degree);
+          float Degree_to_Radaian_cot_45(float degree);
+
+ //^^^^^^^^^^^^^^^^^^^^^^^^for 60 Degree^^^^^^^^^^^^^^^^^^^^^^^^
+
+          float Degree_to_Radaian_sin_60(float degree);
+          float Degree_to_Radaian_cos_60(float degree);
+          float Degree_to_Radaian_tan_60(float degree);
+          float Degree_to_Radaian_cosec_60(float degree);
+          float Degree_to_Radaian_sec_60(float degree);
+          float Degree_to_Radaian_cot_60(float degree);
+
+ //^^^^^^^^^^^^^^^^^^^^^^^^for 90 Degree^^^^^^^^^^^^^^^^^^^^^^^^
+
+          float Degree_to_Radaian_sin_90(float degree);
+          float Degree_to_Radaian_cos_90(float degree);
+          float Degree_to_Radaian_tan_90(float degree);
+          float Degree_to_Radaian_cosec_90(float degree);
+          float Degree_to_Radaian_sec_90(float degree);
+          float Degree_to_Radaian_cot_90(float degree);
