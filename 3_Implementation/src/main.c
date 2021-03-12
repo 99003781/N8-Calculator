1e980d3f974301c9fefe4d52308bc6158e405640
99003781 <78873487+99003781@users.noreply.github.com>
2021-03-12 14:58:24 +0530

    Update main.c

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -1,21 +1,109 @@
 #include "header_main.h"
 
-//###################Trigonometary##################
+//###################Trigonometary###################
+   
+   //**********Function for Degree_to_Radaian conversion************
 
-int get_user_choice();
-int Check_selected_value(int input);
-int Check_selected_valuexx(int input);
-float Degree_to_Radian();
-float Radian_to_Degree();
-int check();
+   float Degree_to_Radaian_sin(float degree);
+   float Degree_to_Radaian_cos(float degree);
+   float Degree_to_Radaian_tan(float degree);
+   float Degree_to_Radaian_cosec(float degree);
+   float Degree_to_Radaian_sec(float degree);
+   float Degree_to_Radaian_cot(float degree);
+
+   //**********Function for Radaian_to_Degree conversion************
+   
+   float Radaian_to_Degree_sin(float radian);
+   float Radaian_to_Degree_cos(float radian);
+   float Radaian_to_Degree_tan(float radian);
+   float Radaian_to_Degree_cosec(float radian);
+   float Radaian_to_Degree_sec(float radian);
+   float Radaian_to_Degree_cot(float radian);
 
 
-/*----------------function 1-------------------- */
+ //**********Function for Degree_to_Radaian conversion************
 
-int get_user_choice()
+float Degree_to_Radaian_sin(float degree)
 {
-int selected_choice; 
-   //scanf("%d",&selected_choice);
-//scanf("%d",&selected_choice);
-return selected_choice;
+   int radian;
+radian = degree*(pi/180.0);
+   return radian;
 }
+
+float Degree_to_Radaian_cos(float degree)
+{
+   int radian;
+radian = degree*(pi/180.0);
+   return radian;
+}
+
+float Degree_to_Radaian_tan(float degree)
+{
+   int radian;
+radian = degree*(pi/180.0);
+   return radian;
+}
+
+float Degree_to_Radaian_cosec(float degree)
+{
+   int radian;
+radian = degree*(pi/180.0);
+   return radian;
+}
+
+float Degree_to_Radaian_sec(float degree)
+{
+   int radian;
+radian = degree*(pi/180.0);
+   return radian;
+}
+float Degree_to_Radaian_cot(float degree)
+{
+   int radian;
+radian = degree*(pi/180.0);
+   return radian;
+}
+
+
+//**********Function for Radaian_to_Degree conversion************
+
+
+float Radaian_to_Degree_sin(float radain)
+{
+   int degree
+degree = radian/(pi/180.0);
+   return degree;
+   
+float Radaian_to_Degree_cos(float radain)
+{
+   int degree
+degree = radian/(pi/180.0);
+   return degree;
+      
+float Radaian_to_Degree_cot(float radain)
+{
+   int degree
+degree = radian/(pi/180.0);
+   return degree;
+         
+float Radaian_to_Degree_cosec(float radain)
+{
+   int degree
+degree = radian/(pi/180.0);
+   return degree;
+            
+float Radaian_to_Degree_sec(float radain)
+{
+   int degree
+degree = radian/(pi/180.0);
+   return degree;
+               
+float Radaian_to_Degree_cot(float radain)
+{
+   int degree
+degree = radian/(pi/180.0);
+   return degree;
+   
+   
+
+
