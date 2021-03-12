1d29011ca50e6c25121000d6ca064bf24e2ecebf
99003781 <78873487+99003781@users.noreply.github.com>
2021-03-12 15:15:27 +0530

    Update test_main.c

--- 3_Implementation/test/test_main.c
+++ 3_Implementation/test/test_main.c
@@ -14,7 +14,7 @@
           float Degree_to_Radaian_sec_0(float degree);
           float Degree_to_Radaian_cot_0(float degree);
 
- //^^^^^^^^^^^^^^^^^^^^^^^^for 30 Degree^^^^^^^^^^^^^^^^^^^^^^^^
+       //^^^^^^^^^^^^^^^^^^^^^^^^for 30 Degree^^^^^^^^^^^^^^^^^^^^^^^^
 
           float Degree_to_Radaian_sin_30(float degree);
           float Degree_to_Radaian_cos_30(float degree);
@@ -23,7 +23,7 @@
           float Degree_to_Radaian_sec_30(float degree);
           float Degree_to_Radaian_cot_30(float degree);
 
- //^^^^^^^^^^^^^^^^^^^^^^^^for 45 Degree^^^^^^^^^^^^^^^^^^^^^^^^
+      //^^^^^^^^^^^^^^^^^^^^^^^^for 45 Degree^^^^^^^^^^^^^^^^^^^^^^^^
 
           float Degree_to_Radaian_sin_45(float degree);
           float Degree_to_Radaian_cos_45(float degree);
@@ -32,7 +32,7 @@
           float Degree_to_Radaian_sec_45(float degree);
           float Degree_to_Radaian_cot_45(float degree);
 
- //^^^^^^^^^^^^^^^^^^^^^^^^for 60 Degree^^^^^^^^^^^^^^^^^^^^^^^^
+      //^^^^^^^^^^^^^^^^^^^^^^^^for 60 Degree^^^^^^^^^^^^^^^^^^^^^^^^
 
           float Degree_to_Radaian_sin_60(float degree);
           float Degree_to_Radaian_cos_60(float degree);
@@ -41,7 +41,7 @@
           float Degree_to_Radaian_sec_60(float degree);
           float Degree_to_Radaian_cot_60(float degree);
 
- //^^^^^^^^^^^^^^^^^^^^^^^^for 90 Degree^^^^^^^^^^^^^^^^^^^^^^^^
+      //^^^^^^^^^^^^^^^^^^^^^^^^for 90 Degree^^^^^^^^^^^^^^^^^^^^^^^^
 
           float Degree_to_Radaian_sin_90(float degree);
           float Degree_to_Radaian_cos_90(float degree);
@@ -49,3 +49,57 @@
           float Degree_to_Radaian_cosec_90(float degree);
           float Degree_to_Radaian_sec_90(float degree);
           float Degree_to_Radaian_cot_90(float degree);
+
+
+ //**********test cases for Radaian_to_Degree conversion************
+
+          //^^^^^^^^^^^^^^^^^^^^^^^^for 0 Radain^^^^^^^^^^^^^^^^^^^^^^^^
+
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+
+            //^^^^^^^^^^^^^^^^^^^^^^^^for PI/6 Radain^^^^^^^^^^^^^^^^^^^^^^^^
+
+            float Radaian_to_Degree_sin_PI/6(float radian);
+            float Radaian_to_Degree_sin_PI/6(float radian);
+            float Radaian_to_Degree_sin_PI/6(float radian);
+            float Radaian_to_Degree_sin_PI/6(float radian);
+            float Radaian_to_Degree_sin_PI/6(float radian);
+            float Radaian_to_Degree_sin_PI/6(float radian);
+
+
+            //^^^^^^^^^^^^^^^^^^^^^^^^for PI/4 Radain^^^^^^^^^^^^^^^^^^^^^^^^
+
+            float Radaian_to_Degree_sin_PI/4(float radian);
+            float Radaian_to_Degree_sin_PI/4(float radian);
+            float Radaian_to_Degree_sin_PI/4(float radian);
+            float Radaian_to_Degree_sin_PI/4(float radian);
+            float Radaian_to_Degree_sin_PI/4(float radian);
+            float Radaian_to_Degree_sin_PI/4(float radian);
+
+
+            //^^^^^^^^^^^^^^^^^^^^^^^^for 0 Radain^^^^^^^^^^^^^^^^^^^^^^^^
+
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+
+
+            //^^^^^^^^^^^^^^^^^^^^^^^^for 0 Radain^^^^^^^^^^^^^^^^^^^^^^^^
+
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+            float Radaian_to_Degree_sin_0(float radian);
+
+
+            
