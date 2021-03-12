6b9981f8800095e18ae9e4bd506645b581f1a554
99003781 <78873487+99003781@users.noreply.github.com>
2021-03-12 14:00:19 +0530

    Update main.c

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -1,1 +1,21 @@
 #include "header_main.h"
+
+//###################Trigonometary##################
+
+int get_user_choice();
+int Check_selected_value(int input);
+int Check_selected_valuexx(int input);
+float Degree_to_Radian();
+float Radian_to_Degree();
+int check();
+
+
+/*----------------function 1-------------------- */
+
+int get_user_choice()
+{
+int selected_choice; 
+   = scanf("%d",&selected_choice);
+//scanf("%d",&selected_choice);
+return selected_choice;
+}
