bb3666a5cdd89b16e5aaf424ef77a09bf5d3f280
99003781 <78873487+99003781@users.noreply.github.com>
2021-03-12 14:15:49 +0530

    Update test_main.c

--- 3_Implementation/test/test_main.c
+++ 3_Implementation/test/test_main.c
@@ -1,3 +1,10 @@
 #include "unity.h"
 #include "header_main.h"
 
+//####################trigonometary############
+
+
+void test_get_user_choice(void)
+{
+     TEST_ASSERT_EQUAL(1,get_user_choice());
+}
