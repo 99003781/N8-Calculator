9651c45a8aedd3637914d19302f791a7b17efe3a
99003781 <78873487+99003781@users.noreply.github.com>
2021-03-13 16:56:10 +0530

    Add files via upload

---
+++ 3_Implementation/inc/AREA.h
@@ -0,0 +1,13 @@
+
+#ifndef __AREA_H__
+#define __AREA_H__
+
+#include <stdio.h>
+
+int rect_area(int length,int width);
+int square_area(int length);
+int triangle_area(int length,int width);
+int circum_area(double radius);
+
+#endif
+
